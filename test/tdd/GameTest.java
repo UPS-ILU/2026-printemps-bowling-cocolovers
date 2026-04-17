84d5fc2dd1842c9335fba652e11f05ee4fe28a41
cocolovers <mdieunguyen2912@gmail.com>
2026-04-17 15:28:13 +0200

    Test spare (cont)

--- test/tdd/GameTest.java
+++ test/tdd/GameTest.java
@@ -27,5 +27,26 @@ class GameTest {
 			game.roll(1);
 		assertEquals(20, game.score());
 	}
+	
+	@Test
+	void test_dix_un() {
+		for (int i=0;i<10;i++)
+			game.roll(1);
+		for (int i=0;i<10;i++)
+			game.roll(2);
+		assertEquals(30, game.score());
+	}
+	
+	@Test
+	void test_spare() {
+		game.roll(7);
+		game.roll(3);
+		game.roll(4);
+		for (int i=0;i<17;i++)
+			game.roll(0);
+		assertEquals(18, game.score());
+	}
+	
+	
 
 }
